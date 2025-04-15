908af50972e6c8ac5f0b2e7d638e107500c75dda
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-15 09:21:13 -0500

    Clase 4: arrow functions

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,19 @@
+
+// Declarar una función de forma tradicional
+
+function  square1(num) {
+    return num*5
+}
+
+// Declarar una función: arrow function
+
+const square2 = (num) => {
+    return num*5
+}
+
+const square3 = num => num * num
+
+let resultado = square3(5)
+
+
+console.log(resultado)
