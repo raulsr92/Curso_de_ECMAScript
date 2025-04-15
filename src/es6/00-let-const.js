1283038c314c4ee4fa0a25fdf54c6329ab2f77df
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-15 08:51:57 -0500

    Clase 4: aprendiendo a usar let y const y block scope

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,46 @@
+// = === = = = = == = = = = = = = = = = =  Declarar y asignar con VAR
+
+var lastName = "Raul";
+
+// Reasignar
+
+lastName = "Adolfo"
+
+console.log(lastName);
+
+// = === = = = = == = = = = = = = = = = = Declarar y asignar  con LET
+
+let fruit = "manzana"
+
+// Reasignar
+
+fruit = "maracuyá"
+
+console.log(fruit);
+
+// = === = = = = == = = = = = = = = = = = Declarar y asignar  con CONST
+
+const team = "Manchester United"
+
+team = "Inter de Milán"
+
+console.log(team);
+
+// = === = = = = == = = = = = = = = = = = 
+
+const fruits = () =>{ 
+    if(true){
+        var fruit1 = "Manzana";
+        let fruit2 = "Plátano";
+        const fruit3 = "Naranja";
+
+    }
+
+    console.log(fruit1)
+    console.log(fruit2)
+    console.log(fruit3)
+
+}
+
+fruits()
+
